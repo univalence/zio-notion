81ded6dd5d919f39527cff8bd0e2ac3797bf8f14
Jonathan Winandy <jonathan.winandy@gmail.com>
2022-05-02 11:52:41 +0200

    refacto: clean

--- zio-notion-core/src/main/scala/zio/notion/model/Link.scala
+++ zio-notion-core/src/main/scala/zio/notion/model/Link.scala
@@ -4,5 +4,7 @@ import io.circe.generic.extras._
 
 @ConfiguredJsonCodec sealed trait Link
 
-final case class File(name: String, file: ExpirableUrl) extends Link
-final case class External(name: String, external: Url)  extends Link
+object Link {
+  final case class File(name: String, file: ExpirableUrl) extends Link
+  final case class External(name: String, external: Url) extends Link
+}
