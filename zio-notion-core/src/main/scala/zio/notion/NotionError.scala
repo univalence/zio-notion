126016481b5b6005e798bc734d53b8c02ed8ca60
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2022-05-11 14:18:22 +0200

    refactor: improve error reporting (#13)

--- zio-notion-core/src/main/scala/zio/notion/NotionError.scala
+++ zio-notion-core/src/main/scala/zio/notion/NotionError.scala
@@ -6,26 +6,32 @@ sealed trait NotionError extends Throwable {
   def humanize: String
 }
 
-final case class ConnectionError(throwable: Throwable) extends NotionError {
-  override def humanize: String = throwable.getMessage
-}
+object NotionError {
+  final case class ConnectionError(throwable: Throwable) extends NotionError {
+    override def humanize: String = throwable.getMessage
+  }
 
-final case class HttpError(code: Int, message: String) extends NotionError {
-  override def humanize: String = s"$code: $message"
-}
+  final case class HttpError(curl: String, status: Int, code: String, message: String) extends NotionError {
+    override def humanize: String =
+      s"""$message
+         |
+         |The following request to Notion failed with a status $status corresponding to "$code":
+         |${curl.split("\n").map(" > " + _).mkString("\n")}""".stripMargin
+  }
 
-final case class JsonError(error: CirceError) extends NotionError {
-  override def humanize: String = error.getMessage
-}
+  final case class JsonError(error: CirceError) extends NotionError {
+    override def humanize: String = error.getMessage
+  }
 
-final case class PropertyNotExist(propertyName: String, pageId: String) extends NotionError {
-  override def humanize: String = s"Property $propertyName doesn't exists for $pageId."
-}
+  final case class PropertyNotExist(propertyName: String, pageId: String) extends NotionError {
+    override def humanize: String = s"Property $propertyName doesn't exists for $pageId."
+  }
 
-final case class PropertyWrongType(propertyName: String, expectedType: String) extends NotionError {
-  override def humanize: String = s"Property $propertyName can't be updated, expected $expectedType type."
-}
+  final case class PropertyWrongType(propertyName: String, expectedType: String, foundType: String) extends NotionError {
+    override def humanize: String = s"Property named $propertyName can't be updated, expected a $expectedType but found a $foundType."
+  }
 
-final case class PropertyIsEmpty(propertyName: String) extends NotionError {
-  override def humanize: String = s"Property $propertyName can't be updated because is is empty."
+  final case class PropertyIsEmpty(propertyName: String) extends NotionError {
+    override def humanize: String = s"Property $propertyName can't be updated because is is empty."
+  }
 }
