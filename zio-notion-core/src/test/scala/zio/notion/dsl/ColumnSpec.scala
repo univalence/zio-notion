f4da7d59bf82224bd14e406bcaf8da8ca88d621a
dylandoamaral <do.amaral.dylan@gmail.com>
2022-05-23 11:32:25 +0200

    feat: add more tests

--- zio-notion-core/src/test/scala/zio/notion/dsl/ColumnSpec.scala
+++ zio-notion-core/src/test/scala/zio/notion/dsl/ColumnSpec.scala
@@ -1,11 +1,12 @@
 package zio.notion.dsl
 
 import zio.Scope
+import zio.notion.model.database.patch.PatchPlan
 import zio.test._
 
 object ColumnSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Any] =
-    suite("Colmun suite")(
+    suite("Column suite")(
       test("I can generate a column using string context") {
         val column = $"col1"
 
@@ -15,6 +16,11 @@ object ColumnSpec extends ZIOSpecDefault {
         val column = col("col1")
 
         assertTrue(column == Column("col1"))
+      },
+      test("I can convert a column into a column definition") {
+        val columnDefinition = col("col1").definition
+
+        assertTrue(columnDefinition == ColumnDefinition("col1", PatchPlan.unit))
       }
     )
 }
