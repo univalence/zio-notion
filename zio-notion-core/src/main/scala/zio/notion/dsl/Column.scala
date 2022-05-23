acdb14f971b9db36260f8614580dd79e049823c9
dylandoamaral <do.amaral.dylan@gmail.com>
2022-05-23 10:18:31 +0200

    fix: formatting

--- zio-notion-core/src/main/scala/zio/notion/dsl/Column.scala
+++ zio-notion-core/src/main/scala/zio/notion/dsl/Column.scala
@@ -3,7 +3,7 @@ package zio.notion.dsl
 import zio.notion.model.database.query.Sorts.Sorting
 import zio.notion.model.database.query.Sorts.Sorting.Property
 
-case class Column(name: String) {
+final case class Column(name: String) {
   def ascending: Sorting  = Property(name, ascending = true)
   def descending: Sorting = Property(name, ascending = false)
 }
