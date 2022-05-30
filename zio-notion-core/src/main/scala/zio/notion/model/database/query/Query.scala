d9c9c6a6e299344f3ddc5d43a29fbbbc0a1cac4b
guihardbastien <bastien_guihard@orange.fr>
2022-05-30 17:00:30 +0200

    feat: add empty queries

--- zio-notion-core/src/main/scala/zio/notion/model/database/query/Query.scala
+++ zio-notion-core/src/main/scala/zio/notion/model/database/query/Query.scala
@@ -1,12 +1,13 @@
 package zio.notion.model.database.query
 
 import io.circe.Encoder
-import io.circe.generic.semiauto.deriveEncoder
+
+import zio.notion.model.magnolia.NoDiscriminantNoNullEncoderDerivation
 
 final case class Query(filter: Option[Filter], sorts: Option[Sorts])
 
 object Query {
   val empty: Query = Query(None, None)
 
-  implicit val encoder: Encoder[Query] = deriveEncoder[Query]
+  implicit val encoder: Encoder[Query] = NoDiscriminantNoNullEncoderDerivation.gen[Query] // deriveEncoder[Query]
 }
