cd6dc06563a66e94bc17df74dcbea9cceec1a7af
dylandoamaral <do.amaral.dylan@gmail.com>
2022-06-20 16:52:20 +0200

    refactor: generalize notion live client

--- zio-notion-core/src/main/scala/zio/notion/package.scala
+++ zio-notion-core/src/main/scala/zio/notion/package.scala
@@ -1,10 +1,13 @@
 package zio
 
+import sttp.client3.Request
+
 import zio.notion.NotionError.ParsingError
 import zio.notion.model.page.Property
 import zio.prelude.Validation
 
 package object notion {
+  type NotionRequest = Request[Either[String, String], Any]
 
   def decodePropertiesAs[A](properties: Map[String, Property])(implicit A: Converter[A]): Validation[ParsingError, A] =
     A match {
