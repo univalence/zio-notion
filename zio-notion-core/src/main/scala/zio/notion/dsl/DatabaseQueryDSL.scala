5a44433fcbcfa9596a3b9de763afa7558e96dcdd
dylandoamaral <do.amaral.dylan@gmail.com>
2022-08-02 14:44:05 +0200

    fix: timestamp sorting serialization

--- zio-notion-core/src/main/scala/zio/notion/dsl/DatabaseQueryDSL.scala
+++ zio-notion-core/src/main/scala/zio/notion/dsl/DatabaseQueryDSL.scala
@@ -6,17 +6,18 @@ import zio.notion.model.database.query.Sorts.Sorting._
 import zio.notion.model.database.query.Sorts.Sorting.TimestampType.{CreatedTime, LastEditedTime}
 
 trait DatabaseQueryDSL {
-  implicit def sortsToQuery(sorts: Sorts): Query = Query(None, Some(sorts))
-
+  implicit def sortsToQuery(sorts: Sorts): Query    = Query(None, Some(sorts))
   implicit def filterToQuery(filter: Filter): Query = Query(Some(filter), None)
 
   // Sort helpers
 
   implicit def timestampTypeToSort(timestampType: TimestampType): Sorts = Timestamp(timestampType, ascending = true)
+  implicit def columnToSort(column: Column): Sorts                      = Property(column.colName, ascending = true)
+  implicit def sortingToSort(sorting: Sorting): Sorts                   = Sorts(List(sorting))
 
-  implicit def columnToSorting(column: Column): Sorts = Property(column.colName, ascending = true)
-
-  implicit def sortingToSort(sorting: Sorting): Sorts = Sorts(List(sorting))
+  implicit def timestampTypeToQuery(timestampType: TimestampType): Query = sortsToQuery(timestampTypeToSort(timestampType))
+  implicit def columnToQuery(column: Column): Query                      = sortsToQuery(columnToSort(column))
+  implicit def sortingToQuery(sorting: Sorting): Query                   = sortsToQuery(sortingToSort(sorting))
 
   val byCreatedTime: TimestampType    = CreatedTime
   val byLastEditedTime: TimestampType = LastEditedTime
